1f091cd773428d358e43095fc9229ca883b447d9
CarlosGongorar <carlosgongorar50@gmail.com>
2024-12-20 18:43:36 -0500

    1 Commit adding JSON data

--- app/page.jsx
+++ app/page.jsx
@@ -1,11 +1,24 @@
-function Homepage() {
-  return (
-    <div>
-        <h1>
-          Homepage
-        </h1>
-        
-    </div>
+async function fetchData() {
+  const res = await fetch("https://reqres.in/api/users")
+  const data = await res.json()
+  console.log(data)
+  return data.data
+}
+
+async function Homepage() {
+  const users = await fetchData()
+  
+  return(
+    <ul>
+      {users.map(user =>(
+        <li key={user.id}>
+          <div>
+            <h1>{user.id} {user.first_name} </h1>
+            <p>Email: {user.email}</p>
+          </div>
+        </li>
+      ))}
+    </ul>
   )
 }
 
